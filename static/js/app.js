29236109e29cae411910a803a38ebb4f0ae92ca3
Todd Paulsen <toddpaulsen@gmail.com>
2018-10-01 11:28:08 -0400

    finished row functionality to show shows rows of cards TODO: add margins between cards.  Removed cards placeholders from index.html.

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,10 @@ function showProducts(response) {
   let html = "";
   // loop through products and create card for each
   for (let i=0; i<products.length; i++) {
-
+    // open row for each third product
+    if (i % 3 == 0) {
+      html += '<div class="row">'
+    }
     html += `
     <div class="card col-md-4">
       <div class="card-img-top">
@@ -25,6 +28,11 @@ function showProducts(response) {
       <button onclick="addToCart(${products[i].id})" class="btn btn-primary">Add To Cart</button>
     </div> <!-- End of card -->
     `
+    // close row after three products have been added
+    // because 0,1,2 is a row, use i+1 to check if your are at the end of the row
+    if ((i + 1) % 3 == 0) {
+      html += '</div>'
+    }
   }
   // inject html string into products using the html variable we created above
   $("#products").html(html);
